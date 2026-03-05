91d5486e02c01ec881579ffbb500da94599ec2fd
chuckerin <chuckerin@gmail.com>
2026-03-05 13:17:22 -0600

    moreso home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <p>Small app using React Router 6.4</p>
+      <p>Small webapp using React Router 6.4</p>
       <p>To get started, select Users from menu</p>
     </div>
   );
